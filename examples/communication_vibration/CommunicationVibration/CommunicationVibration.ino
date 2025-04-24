19e766e810e43010af39fd1138c9419a231417ed
alexanderreiprich <al3xz0n3@gmail.com>
2025-04-24 17:42:33 +0200

    setup: folder structure

---
+++ examples/communication_vibration/CommunicationVibration/CommunicationVibration.ino
@@ -0,0 +1,11 @@
+#include "Dezibot.h"
+
+Dezibot dezibot = Dezibot();
+void setup() {
+  // put your setup code here, to run once:
+  dezibot.begin();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
