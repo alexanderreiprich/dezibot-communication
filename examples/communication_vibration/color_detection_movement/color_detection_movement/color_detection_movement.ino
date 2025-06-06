a3c573f1c1beb5529f816b1237d86a23cae9a50e
Anna Denzel <anna.denzel@gmx.de>
2025-06-06 12:58:37 +0200

    color movement improvement

--- examples/communication_vibration/color_detection_movement/color_detection_movement/color_detection_movement.ino
+++ examples/communication_vibration/color_detection_movement/color_detection_movement/color_detection_movement.ino
@@ -1,6 +1,6 @@
 #include <Dezibot.h>
 
-#define READ_DELAY 1000
+#define READ_DELAY 100
 
 Dezibot dezibot = Dezibot();
 
@@ -17,29 +17,40 @@ void loop() {
   uint16_t colorValueBlue = dezibot.colorDetection.getColorValue(VEML_BLUE);
   uint16_t colorValueGreen = dezibot.colorDetection.getColorValue(VEML_GREEN);
   uint16_t colorValueWhite = dezibot.colorDetection.getColorValue(VEML_WHITE);
+  float colorGreenWhiteRatio = float(colorValueGreen) / float(colorValueWhite);
   
   printValue(VEML_RED, "R");
   printValue(VEML_GREEN, "G");
   printValue(VEML_BLUE, "B");
   printValue(VEML_WHITE, "W");
 
-  if ((colorValueRed > colorValueGreen) && (colorValueRed > colorValueBlue) && (colorValueRed > colorValueWhite / 2)) {
-    dezibot.display.print("red, move clockwise");
-    dezibot.motion.rotateClockwise(500);
-  }
-  else if ((colorValueGreen > colorValueRed) && (colorValueGreen > colorValueBlue) && (colorValueGreen > colorValueWhite / 2)) {
-    dezibot.display.print("green, move anti-clockwise");
-    dezibot.motion.rotateAntiClockwise(500);
-  }
-  else if ((colorValueBlue > colorValueRed) && (colorValueBlue > colorValueGreen) && (colorValueBlue > colorValueWhite / 3)) {
-    dezibot.display.print("blue, move both");
-    dezibot.motion.move(500);
-  }
-  else {
-    dezibot.display.print("white, stop moving");
+  if (colorValueWhite < 1200){
+    dezibot.display.print("silent mode");
     dezibot.motion.stop();
   }
-  delay(READ_DELAY);
+   else if ((colorValueRed > colorValueGreen) && (colorValueRed > colorValueBlue) && (colorValueRed > colorValueWhite / 2)) {
+    dezibot.display.print("red, left turn");
+    leftTurn();
+  }
+  else if ((colorValueGreen > colorValueRed) && (colorValueGreen > colorValueBlue) && (colorGreenWhiteRatio > float(0.7))) {
+    dezibot.display.print("green");
+    rightTurn();
+  }
+  else if ((colorValueBlue > colorValueRed) && (colorValueBlue > colorValueGreen) && (colorValueBlue > colorValueWhite / 2)) {
+    dezibot.display.print("blau");
+    dezibot.display.print((colorValueWhite / 2 ) - colorValueBlue);
+    moveForward();
+  }
+  // else if ((colorValueBlue + colorValueRed + colorValueGreen) * float(0.64) < colorValueWhite){
+    else if (colorValueWhite > 4000){
+    dezibot.display.print("white");
+    dezibot.display.print((colorValueBlue + colorValueRed + colorValueGreen) * 2 / 3 ); 
+    dezibot.motion.stop();
+  } else {
+    dezibot.display.print("delay");
+    dezibot.display.print((colorValueBlue + colorValueRed + colorValueGreen) * 2 / 3 ); 
+    delay(READ_DELAY);
+  }
 }
 
 void printValue(color color, String prefix) {
@@ -52,4 +63,22 @@ void printValue(color color, String prefix) {
   Serial.print(prefix);
   Serial.print(" ");
   Serial.println(colorValue);
+}
+
+void leftTurn() {
+  dezibot.motion.stop();
+  dezibot.motion.right.setSpeed(8192);
+  delay(READ_DELAY);
+}
+
+void rightTurn() {
+  dezibot.motion.stop();
+  dezibot.motion.left.setSpeed(8192);
+  delay(READ_DELAY*2);
+}
+
+void moveForward() {
+  dezibot.motion.stop();
+  dezibot.motion.move(0, 5000);
+  delay(READ_DELAY);
 }
